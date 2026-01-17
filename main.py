4705f18ed967630087376c04f33f4efe036ef116
dcaridelsonbalbinodesena2025-lang <dc.aridelsonbalbinodesena2025@gmail.com>
2026-01-17 12:32:44 -0300

    main.py

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
 from flask_socketio import SocketIO
-from iqoptionapi.stable_api import IQ_Option
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Suas credenciais e SSID
-SSID = "A_OEQ0BLQUmg239qW"
-
 @app.route('/')
 def index():
-    return "Ponte IQ Option Ativa!"
-
-@socketio.on('connect')
-def handle_connect():
-    print("Cliente conectado à ponte IQ Option")
+    return "Ponte IQ Option Ativa e Online!"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
